f1652c9d1887c41a63a01edde8e8b73ddf8b88fb
Ross Starkey <ross.starkey@asm.org.uk>
2019-04-01 14:21:06 +0100

    Added auto complete control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { filter, map, startWith } from 'rxjs/operators';
+
+export interface State {
+  declarationTypeCode: string;
+  declarationTypeCodeDescription: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'HeliosControlPlayGround';
-}
+    stateCtrl = new FormControl();
+    filteredStates: Observable<State[]>;
+
+
+    @Input() panelWidth: string | number;
+
+    states: State[] = [
+      {
+        declarationTypeCode: 'IMA',
+        declarationTypeCodeDescription: 'Standard customs declaration (Import not EFTA) - Goods arrived',
+      },
+      {
+        declarationTypeCode: 'IMB',
+        declarationTypeCodeDescription: 'Simplified declaration on an occasional basis (Import not EFTA) - Goods arrived',
+      },
+      {
+        declarationTypeCode: 'IMD',
+        declarationTypeCodeDescription: 'Standard customs declaration (Import not EFTA) - Goods not arrived',
+      },
+      {
+        declarationTypeCode: 'COB',
+        declarationTypeCodeDescription: 'Simplified declaration on an occasional basis (Trade new Member States) - Goods arrived',
+      }
+    ];
+
+    constructor() {
+      this.filteredStates = this.stateCtrl.valueChanges
+        .pipe(
+          startWith(''),
+          map(state => state ? this._filterStates(state) : this.states.slice())
+        );
+    }
+
+    private _filterStates(value: string): State[] {
+      const filterValue = value.toLowerCase();
+
+      return this.states.filter(state => state.declarationTypeCode.toLowerCase().indexOf(filterValue) === 0);
+    }
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatNativeDateModule } from '@angular/material';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 @NgModule({
   declarations: [
@@ -10,9 +16,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MaterialModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
